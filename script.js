c2e832ece2fe95912efd9d5284e7d6c8837fe21f
jdeniau28 <je.deniau@laposte.net>
2022-03-23 11:32:50 +0100

    propriété sur les icones + id à la banniere + ajout script.js avec function scroll

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    scrollFunction()
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20) {
+        document.getElementById(".hearder").style.display = "none";
+        // document.getElementById(".header").style.position = "initial";
+    } else {
+        document.getElementById(".header").style.display = "block";
+        // document.getElementById(".header").style.position = "fixed";
+    }
+};
